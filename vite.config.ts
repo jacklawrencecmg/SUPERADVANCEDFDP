957327f93503d94dff562154cfeba8ccac5a53a1
Jack Lawrence <jlawrence@cmgfi.com>
2026-03-05 22:06:51 -0500

    Fix: use fixed JS filename to prevent CDN cache mismatch on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'assets/app-[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      }
+    }
+  }
 })
